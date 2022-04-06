da63b5ba873b31c099ae7fef0fd393b16b9111e3
laffont emile <emile.laffont@gmail.com>
2022-04-06 08:50:10 +0200

    Ajout d'un squelette basique pour la mise en plkace de l'integration continue

---
+++ src/Dataframe.java
@@ -0,0 +1,18 @@
+
+
+public class Dataframe {
+
+	private int n;
+
+	public Dataframe(int n){
+		this.n = n;
+	}
+	
+	public int valeur(){
+		return n;
+	}
+
+	public static main(String[] args){
+		System.out.println("Hello");
+	}
+}
