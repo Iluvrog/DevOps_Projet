2ec8b1b3916088450761a70422599c5d7b495f4c
laffont emile <emile.laffont@gmail.com>
2022-04-06 09:28:28 +0200

    ajout de maven

---
+++ dataframe/src/test/java/dataframe/DataframeTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Timeout;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DataframeTest {
+
+	//test simple sur les methodes
+	@Test
+	//@Timeout(1)
+	@DisplayName("get interger")
+	public void testBasique() {
+		Dataframe d = new Dataframe(10);
+		assertEquals( 10, d.valeur());
+	}
+}
